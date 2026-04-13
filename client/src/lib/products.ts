43d02e97f0daea08c9368faf1fb9599429e820fb
unknown <deliciasdaamanda1989@gmail.com>
2026-04-13 20:42:24 -0300

    add pratos

--- client/src/lib/products.ts
+++ client/src/lib/products.ts
@@ -82,7 +82,7 @@ export const products: Product[] = [
     category: 'marmitas-trad',
     image: '/images/linguiça.jpg',
     hasOptions: false,
-    availableDays: ['quinta']
+    availableDays: ['segunda']
   },
 
   {
@@ -187,7 +187,7 @@ export const products: Product[] = [
     category: 'marmitas-trad',
     image: '/images/bifeacavalo.jpg',
     hasOptions: false,
-    availableDays: ['terca', 'quarta', 'quinta', 'sexta', 'sabado']
+    availableDays: ['segunda','terca', 'quarta', 'quinta', 'sexta', 'sabado']
   },
   {
     id: 'Calabresa acebolada',
