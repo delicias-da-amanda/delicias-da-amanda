1a7d310f2b6dbab5accb884c99e9e1b1b15d57ee
unknown <deliciasdaamanda1989@gmail.com>
2026-07-06 19:54:51 -0300

    add pratos

--- client/src/lib/products.ts
+++ client/src/lib/products.ts
@@ -440,7 +440,6 @@ export const products: Product[] = [
     category: 'sobremesas',
     image: '/images/prestigio.jpg',
     hasOptions: false,
-    active: false,
     availableDays: ['segunda', 'terca', 'quarta', 'quinta', 'sexta', 'sabado']
   },
    {
